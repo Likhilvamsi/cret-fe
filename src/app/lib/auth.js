5841161a618d2bd1f3b363c1db80e2389b596924
Likhilvamsi <likhilvamsi@ameyait.com>
2025-11-25 18:32:38 +0530

    Update auth.js

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -1,6 +1,6 @@
 // lib/api/auth.js (UPDATED)
 
-const BASE_URL = "http://52.247.225.119/api";
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export async function collegeLogin(payload) {
   const res = await fetch(`${BASE_URL}/auth/login`, {
