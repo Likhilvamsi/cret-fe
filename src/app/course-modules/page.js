751e9f10c608a4682b5675d237fd15839dd15eb6
Likhilvamsi <likhilvamsi@ameyait.com>
2025-11-25 16:00:41 +0530

    Update page.js

--- src/app/course-modules/page.js
+++ src/app/course-modules/page.js
@@ -1,8 +1,6 @@
 "use client";
 export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
-export const revalidate = 0;
-export const ssr = false;
+export const revalidate = false;
 
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
